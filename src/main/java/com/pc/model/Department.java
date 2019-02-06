5bc502ce575cf78b2dde7cb7e3717fd7abde3ae0
Naveen Anem <KH2475@kitspl.com>
2019-02-06 11:44:26 +0530

    Removing unnecessary models

--- src/main/java/com/pc/model/Department.java
+++ src/main/java/com/pc/model/Department.java
@@ -2,17 +2,15 @@ package com.pc.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class Status {
+public class Department {
+
 	@JsonProperty(value = "id")
 	private int id;
 
 	@JsonProperty(value = "name")
 	private String name;
 
-	@JsonProperty(value = "category")
-	private String category;
-
-	public Status() {
+	public Department() {
 
 	}
 
@@ -30,14 +28,6 @@ public class Status {
 
 	public void setName(String name) {
 		this.name = name;
-	}
-
-	public String getCategory() {
-		return category;
-	}
-
-	public void setCategory(String category) {
-		this.category = category;
 	}
 
 }
