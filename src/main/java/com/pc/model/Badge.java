5f444b3f08d99d145070f4a162dd1f8545b04082
Naveen Anem <KH2475@kitspl.com>
2019-07-16 17:38:19 +0530

    Profile related badge changes

--- src/main/java/com/pc/model/Badge.java
+++ src/main/java/com/pc/model/Badge.java
@@ -13,8 +13,19 @@ public class Badge {
 	@JsonProperty("description")
 	private String description;
 
+	@JsonProperty("base64Image")
+	private String base64Image;
+
 	public Badge() {
 
+	}
+
+	public String getBase64Image() {
+		return base64Image;
+	}
+
+	public void setBase64Image(String base64Image) {
+		this.base64Image = base64Image;
 	}
 
 	public int getId() {
